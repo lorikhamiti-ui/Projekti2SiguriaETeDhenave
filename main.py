cd85d6be175af40bff3059ba711609336123ad2b
Lorenaagaj <lorena.agaj@student.uni-pr.edu>
2026-04-18 23:00:04 +0200

    main zgjedhja 1

---
+++ main.py
@@ -0,0 +1,28 @@
+from bifid import enkripto_bifid, dekripto_bifid
+from permutation import enkripto_permutation, dekripto_permutation
+
+def main():
+    vazhdo = True
+
+    while vazhdo:
+        print("\n--- MENUJA ---")
+        print("1. Bifid Cipher")
+        print("2. Permutation Cipher")
+        print("3. Dil")
+
+        zgjedhja = input("Zgjidhni (1, 2 ose 3):")
+
+        if zgjedhja == '1' or zgjedhja == '2':
+            veprimi = input("Deshironi te Enkriptoni apo Dekriptoni? Shkruaj E ose D:")
+
+            teksti = input("Shkruaj tekstin:")
+            çelsi = input("Shkruaj çelsin:")
+
+            if zgjedhja == '1':
+                if veprimi.upper() == 'E':
+                    rezultati = enkripto_bifid(teksti,çelsi)
+                elif veprimi.upper() == 'D':
+                    rezultati = dekripto_bifid(teksti,çelsi)
+                else:
+                    print("Keni dhene veprim te gabuar.")
+                    continue 

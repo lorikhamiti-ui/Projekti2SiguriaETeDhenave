235c52cce042dc5cbcdeeca3f87667e3ff3036ee
Loridaa <lorida.makolli@student.uni-pr.edu>
2026-04-18 23:09:16 +0200

    Add permutation decryption

--- Permutation.py
+++ Permutation.py
@@ -23,3 +23,51 @@ def enkripto_permutation(teksti, çelsi) :
         teksti_enkriptuar += kolonat[indeksi]
 
     return teksti_enkriptuar
+
+def dekripto_permutation(teksti_enkriptuar, celsi):
+    teksti_enkriptuar = teksti_enkriptuar.upper().replace(" ", "")
+    celsi = celsi.upper()
+    
+    alfabeti = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    renditja = []
+
+    for shkronja_alfabeti in alfabeti:
+        for i in range(len(celsi)):
+            if celsi[i] == shkronja_alfabeti:
+                renditja.append(i)
+
+    numri_kolonave = len(celsi)
+    shkronja_per_kolone =len(teksti_enkriptuar) // numri_kolonave
+    mbetja = len(teksti_enkriptuar) % numri_kolonave
+
+    gjatesia_e_kolonave = []
+    for i in range(numri_kolonave):
+        gjatesia_e_kolonave.append(shkronja_per_kolone)
+
+    for i in range(mbetja):
+        gjatesia_e_kolonave[i] += 1
+
+    kolonat_e_renditura = []
+    for i in range(numri_kolonave):
+        kolonat_e_renditura.append("")
+
+    indeksi_tekstit = 0
+    for indeksi_kolones in renditja:
+        gjatesia = gjatesia_e_kolonave[indeksi_kolones]
+        kolona_aktuale = ""
+        for i in range(gjatesia):
+            kolona_aktuale += teksti_enkriptuar[indeksi_tekstit]
+            indeksi_tekstit += 1
+        kolonat_e_renditura[indeksi_kolones] = kolona_aktuale
+
+    teksti_dekriptuar = ""
+    numri_rreshtave = shkronja_per_kolone
+    if mbetja > 0:
+        numri_rreshtave += 1
+
+    for rreshti in range(numri_rreshtave):
+        for kolona in range(numri_kolonave):
+            if rreshti < len(kolonat_e_renditura[kolona]):
+                teksti_dekriptuar += kolonat_e_renditura[kolona][rreshti]
+
+    return teksti_dekriptuar
